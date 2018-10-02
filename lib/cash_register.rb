f2dbb90075b1f8ead79c108e960fc656002f5f34
Erin Green <erin.y.green@gmail.com>
2018-10-02 18:47:34 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
   
  
   
-  def initialize(employee_discount = 0)
+  def initialize(employee_discount=0)
     @total = 0 
     @employee_discount = employee_discount
     @items = []
