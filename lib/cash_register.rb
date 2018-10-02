fb87238dc5350ad965728ad0eefb8fd67eed05f2
Erin Green <erin.y.green@gmail.com>
2018-10-02 19:21:19 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -28,7 +28,7 @@ class CashRegister
   end
   
   def void_last_transaction
-    if self.last_transaction = true 
+    if self.last_transaction != nil  
     self.total = self.total - self.last_transaction
   end 
   end
